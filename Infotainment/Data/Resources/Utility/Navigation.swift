9f9ca80944fc86f0a88cd10db7b1847c0a93f7f3
Denny Feng <fengdennyy@gmail.com>
2021-04-30 11:47:31 -0700

    Added navigation configurator for coloring

---
+++ Infotainment/Data/Resources/Utility/Navigation.swift
@@ -0,0 +1,22 @@
+//
+//  NavigationExtension.swift
+//  Infotainment
+//
+//  Created by Denny Feng on 4/30/21.
+//
+
+import SwiftUI
+
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+
+}
