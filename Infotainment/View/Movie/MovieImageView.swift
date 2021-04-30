294b50ef831541c58c87ac07c96d4414ee81bd65
Denny Feng <fengdennyy@gmail.com>
2021-04-30 00:26:58 -0700

    can now navigate to MovieDetail page when click on info circle

--- Infotainment/View/Movie/MovieImageView.swift
+++ Infotainment/View/Movie/MovieImageView.swift
@@ -2,47 +2,67 @@
 //  MovieImageView.swift
 //  Infotainment
 //
-//  Created by Denny Feng on 4/27/21.
+//  Created by Denny Feng on 4/29/21.
 //
 
 import SwiftUI
 
 struct MovieImageView: View {
     
-// MARK: Instantiate Movie Model
     let movie: Movie
-    
-// MARK: @StateObject
-// StateObject instance does not get destroyed when the view updates
     @StateObject var imageLoader = ImageLoader()
-    
+
     var body: some View {
         let image = self.imageLoader.image
         VStack(alignment: .leading){
             ZStack{
-                Rectangle().fill(Color.gray.opacity(0.3))
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color.white.opacity(0.3))
                 if(image != nil)
                 {
                     Image(uiImage: image!)
                         .resizable()
+                        .frame(maxWidth:.infinity)
+                        .background(Color.black)
+                        .opacity(0.6)
+    
+                        
                 }// end if
+                
+                
+                NavigationLink(
+                    destination: MovieDetailView(movieID: movie.id)
+                ){
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(.white)
+                    .shadow(color: Color.black.opacity(0.9), radius: 3, x: 0.0, y: 3)
+                }// end NavigationLink
+                .offset(x: 140, y:-140)
+                Text("\(movie.title) | \(movie.score)")
+                    
+                    .font(.primary(.regular, size: 20))
+                    .foregroundColor(.white)
+                    .padding()
+//                    MARK:CHANGE COLOR
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .offset(x: 10, y: 120)
                 
             }// end ZStack
             .aspectRatio(1,contentMode: .fit)
-            .cornerRadius(8)
-            .shadow(radius: 4 )
-            Text(movie.overview).font(.subheadline).fontWeight(.medium).padding(10)
-        }//end VStack
-        .lineLimit(3)
+            .cornerRadius(10)
+            .shadow(radius:10)
+        } // end VStack
         .onAppear{
-            self.imageLoader.loadImage(with: self.movie.backdropURL)
-        }// end onAppear
-        
-    }// end body
-}// end MovieImageView
+            self.imageLoader.loadImage(with: movie.posterPathURL)
+        }
+    } // end body
+}
 
 struct MovieImageView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieImageView(movie: Movie.latestMovie).previewLayout(.fixed(width: 500, height: 500))
+        MovieImageView(movie: Movie.latestMovie)
     }
 }
