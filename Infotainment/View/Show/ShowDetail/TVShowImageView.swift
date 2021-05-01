d818025b07105cc73b4d9542f023e516087e37dd
Denny Feng <fengdennyy@gmail.com>
2021-04-30 19:21:53 -0700

    Created image view for TVShow detail page

---
+++ Infotainment/View/Show/ShowDetail/TVShowImageView.swift
@@ -0,0 +1,54 @@
+//
+//  TVShowImageView.swift
+//  Infotainment
+//
+//  Created by Denny Feng on 4/30/21.
+//
+
+import SwiftUI
+
+struct TVShowImageView: View {
+    let show: Shows
+    @StateObject var imageLoader = ImageLoader()
+
+    var body: some View {
+        let image = self.imageLoader.image
+        VStack(alignment: .leading){
+            ZStack{
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color.white.opacity(0.3))
+                if(image != nil)
+                {
+                    Image(uiImage: image!)
+                        .resizable()
+                        .frame(maxWidth:.infinity)
+                        .background(Color.black)
+                        .opacity(0.6)
+                }// end if
+                NavigationLink(
+                    destination: TVShowDetailView(showID: show.id)
+                ){
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .shadow(color: Color.black.opacity(0.9), radius: 3, x: 0.0, y: 3)
+                }// end NavigationLink
+                .offset(x: 120, y:-100)
+                
+            }// end ZStack
+            .aspectRatio(1,contentMode: .fit)
+            .cornerRadius(10)
+            .shadow(radius:10)
+        } // end VStack
+        .onAppear{
+            self.imageLoader.loadImage(with: show.posterPathURL)
+        }
+    } // end body
+}
+
+struct TVShowImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        TVShowImageView(show: Shows.latestShow)
+    }
+}
