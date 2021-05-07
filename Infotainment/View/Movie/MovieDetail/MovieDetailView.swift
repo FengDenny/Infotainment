d11a461bc6c89f39657228bd9a858f4aba92003a
Denny Feng <fengdennyy@gmail.com>
2021-05-06 23:37:07 -0700

    Update MovieDetailView.swift

--- Infotainment/View/Movie/MovieDetail/MovieDetailView.swift
+++ Infotainment/View/Movie/MovieDetail/MovieDetailView.swift
@@ -23,6 +23,8 @@ struct MovieDetailView: View {
                         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 }// end if
                 
+                
+                
             } // end ZStack
             .background(Color.primaryDarkBackground)
             .offset(y:10)
